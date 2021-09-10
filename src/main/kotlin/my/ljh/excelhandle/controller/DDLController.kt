5f8747d12f704e2800a0cd24a77f9eab30da99e9
LiuJH <2294272131@qq.com>
2021-09-10 22:04:33 +0800

    vbhjku

---
+++ src/main/kotlin/my/ljh/excelhandle/controller/DDLController.kt
@@ -0,0 +1,7 @@
+package my.ljh.excelhandle.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class DDLController {
+}
---
+++ src/main/kotlin/my/ljh/excelhandle/service/DDLService.kt
@@ -0,0 +1,10 @@
+package my.ljh.excelhandle.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class DDLService {
+    @Autowired
+    lateinit var ddlService: DDLService
+}
