8071d46f251e92a510198ac7c73a992d9bd1e788
LiuJH <2294272131@qq.com>
2021-09-10 21:13:48 +0800

    Initial commit

---
+++ src/main/kotlin/my/ljh/excelhandle/ExcelHandleApplication.kt
@@ -0,0 +1,11 @@
+package my.ljh.excelhandle
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExcelHandleApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExcelHandleApplication>(*args)
+}
---
+++ src/test/kotlin/my/ljh/excelhandle/ExcelHandleApplicationTests.kt
@@ -0,0 +1,13 @@
+package my.ljh.excelhandle
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExcelHandleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
